876cf77aa03c4747a5f1417831b54545cc851264
Riya Dattani <riya_dattani@hotmail.com>
2019-02-26 15:51:58 +0000

    prints students names that begin with R and less than 12 characters

--- directory.rb
+++ directory.rb
@@ -4,14 +4,14 @@ def input_students
   #create an empty array
   students = []
   #get the first name
-  name = gets.chomp
+  name = gets.chomp.capitalize
   #while the name is not empty, repeat this code:
   while !name.empty? do
     #add the student hash to the array
     students << {name: name, cohort: :novemeber}
     puts "Now we have #{students.count} students"
     #get another name from the user
-    name = gets.chomp
+    name = gets.chomp.capitalize
   end
   # return the array of students
   students
@@ -24,7 +24,9 @@ end
 
 def print(students)
   students.each_with_index do |student, index|
-    puts "#{index +1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    if student[:name].chars.first == "R" && student[:name].length < 12
+      puts "#{index +1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
   end
 end
 
