871257d778c55357adfae000c9d7dfd1fa13a57c
Riya Dattani <riya_dattani@hotmail.com>
2019-02-26 18:20:08 +0000

    centered output

--- directory.rb
+++ directory.rb
@@ -8,7 +8,7 @@ def input_students
   #while the name is not empty, repeat this code:
   while !name.empty? do
     #add the student hash to the array
-    students << {name: name, cohort: :novemeber}
+    students << {name: name, cohort: :novemeber, country_of_birth: :England, hobbies: :coding}
     puts "Now we have #{students.count} students"
     #get another name from the user
     name = gets.chomp.capitalize
@@ -18,20 +18,20 @@ def input_students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(30)
+  puts "-------------".center(30)
 end
 
 def print(students)
   i = 0
   until i == students.length - 1 do
-    puts "#{students[i][:name]} (#{students[i][:cohort]} cohort)"
+    puts "#{students[i][:name]} (#{students[i][:cohort]} cohort)".center(30)
     i +=1
   end
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{students.count} great students".center(30)
 end
 
 students = input_students
