bafe294823be8af734dc92c80d32633147c91b41
Riya Dattani <riya_dattani@hotmail.com>
2019-02-27 15:55:13 +0000

    only prints a list if more than one student

--- directory.rb
+++ directory.rb
@@ -32,8 +32,10 @@ def print_header
 end
 
 def print(students)
-  students.each_with_index do |student, index|
-    puts "#{index +1}. #{student[:name]} (#{student[:cohort]} cohort)".center(30)
+  if students.count > 1
+    students.each_with_index do |student, index|
+      puts "#{index +1}. #{student[:name]} (#{student[:cohort]} cohort)".center(30)
+    end
   end
 end
 
