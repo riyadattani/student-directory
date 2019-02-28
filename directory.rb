af955407effff6d87dc40aec7f059be72667b58c
Riya Dattani <riya_dattani@hotmail.com>
2019-02-28 10:19:33 +0000

    interactive menu added

--- directory.rb
+++ directory.rb
@@ -1,8 +1,8 @@
+@students = [] #empty array accessible to all methods
+
 def input_students
   puts "Please enter the name of the student"
   puts "To finish, just hit return twice"
-  students = []
-  #get the first name
   name = gets.chomp.capitalize
   #while the name is not empty, repeat this code:
   while !name.empty? do
@@ -12,39 +12,69 @@ def input_students
       month = "TBC".to_sym
     end
     #add the student hash to the array
-    students << {name: name, cohort: month}
-    if students.count == 1
-      puts "Now we have #{students.count} student"
+    @students << {name: name, cohort: month}
+    if @students.count == 1
+      puts "Now we have #{@students.count} student"
     else
-      puts "Now we have #{students.count} students"
+      puts "Now we have #{@students.count} students"
     end
     #get another name from the user
     puts "Enter the name of the student"
     name = gets.chomp.capitalize
   end
-  # return the array of students
-  students
 end
+
+#----------Interactive Menu----------
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I dont know what you meant, try again"
+  end
+end
+
+#----------Methods---------
 
 def print_header
   puts "The students of Villains Academy".center(30)
   puts "-------------".center(30)
 end
 
-def print(students)
-  if students.count > 1
-    students.each_with_index do |student, index|
+def print_students_list
+  if @students.count > 1
+    @students.each_with_index do |student, index|
       puts "#{index +1}. #{student[:name]} (#{student[:cohort]} cohort)".center(30)
     end
   end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students".center(30)
+def print_footer
+  puts "Overall, we have #{@students.count} great students".center(30)
 end
 
-students = input_students
-#nothing happens until we call the methods
-print_header
-print(students)
-print_footer(students)
+interactive_menu
